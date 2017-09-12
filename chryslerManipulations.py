38c10f2963013d100c11e753156aa5ca203962b9
Chris Ernst <chriswernst@gmail.com>
2017-09-11 22:36:48 -0700

    Updates

--- chryslerManipulations.py
+++ chryslerManipulations.py
@@ -11,11 +11,29 @@ import os
 from PIL import Image
 from pylab import *
 
-os.chdir('/Users/ChrisErnst/')
+os.chdir('/Users/ChrisErnst/Development/Python/Image_Manipulation')
 
 pil_im = Image.open('chrysler.jpg')
 # Prepares the image in original form
 
-pil_im = Image.open('chrysler.jpg').convert('l')
+heat_im = Image.open('chrysler.jpg').convert('L')
+# converts to heatmap like image
+
+grey_im = Image.open('chrysler.jpg').convert('LA')
+# converts to greyscale
 
 imshow(pil_im)
+# shows the image in a new window
+
+grey_im.save('greyChrysler.png')
+# saves grey image as a png
+
+pil_im.thumbnail((100,100))
+
+box = (300,300,800,800)
+region = pil_im.crop(box)
+imshow(region)
+# Crops a specific portion of the image
+
+region.rotate(90)
+# Rotates by 90 deg
